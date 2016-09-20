6543afc3cfd52e52b4ebc0a702585453df480ffe
Jan Gosmann <jgosmann@ctngpu1.uwaterloo.ca>
2016-09-20 16:31:28 -0400

    Fix memory task.

--- psy-tasks/task_memory.py
+++ psy-tasks/task_memory.py
@@ -7,16 +7,10 @@ from gosmann_frontiers2016.benchmarks.benchmark import benchmark_memory
 
 
 pspace = (
-    Param(model=['comm_channel']) *
-    Param(n_neurons=[100, 200, 500, 1000]) *
-    Param(dimensions=[1, 2, 5, 10, 20, 50]))
-pspace += (
-    Param(model=['lorenz']) *
-    Param(n_neurons=[100, 200, 500, 1000, 2000]))
-pspace += (
     Param(model=['circ_conv']) *
-    Param(n_neurons=[100, 200, 500]) *
-    Param(dimensions=[5, 10, 20, 50, 100, 200, 500]))
+    Param(n_neurons=[500]) *
+    Param(dimensions=[500]))
+pspace += Param(model=['nback'])
 pspace *= Param(backend=['reference', 'ocl_gpu', 'ocl_cpu', 'optimized'])
 pspace *= Param(trial=range(5))
 
