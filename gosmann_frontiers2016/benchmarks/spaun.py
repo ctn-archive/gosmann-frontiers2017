9492f1545408a05fa926f13cc80c561beeedb773
Jan Gosmann <jan@hyper-world.de>
2016-09-21 11:51:06 -0400

    Make Spaun benchmark actually work.

--- gosmann_frontiers2016/benchmarks/spaun.py
+++ gosmann_frontiers2016/benchmarks/spaun.py
@@ -7,10 +7,23 @@ except:
 import nengo
 import numpy as np
 
-from gosmann_frontiers2016._spaun.main_main import Spaun
+from gosmann_frontiers2016._spaun.configurator import cfg
+from gosmann_frontiers2016._spaun.experimenter import experiment
+from gosmann_frontiers2016._spaun.spaun_main import Spaun
+from gosmann_frontiers2016._spaun.vocabulator import vocab
+from gosmann_frontiers2016._spaun.modules.motor.data import mtr_data
+from gosmann_frontiers2016._spaun.modules.vision.data import vis_data
 
 
 def spaun():
+    def_seq = 'A3[123]?XXXX'
+    cfg.set_seed(1)
+    experiment.initialize(
+        def_seq, vis_data.get_image_ind, vis_data.get_image_label,
+        cfg.mtr_est_digit_response_time, cfg.rng)
+    vocab.initialize(experiment.num_learn_actions, cfg.rng)
+    vocab.initialize_mtr_vocab(mtr_data.dimensions, mtr_data.sps)
+    vocab.initialize_vis_vocab(vis_data.dimensions, vis_data.sps)
     return Spaun()
 
 
--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import find_packages, setup
 
+print(find_packages() + [
+        'gosmann_frontiers2016.' + p for p in find_packages('spaun2.0')])
+
 setup(
     name="gosmann_frontiers2016",
     version="1.0",
     author="Jan Gosmann",
     author_email="jgosmann@uwaterloo.ca",
-    packages=find_packages() + ['gosmann_frontiers2016._spaun'],
+    packages=find_packages() + [
+        'gosmann_frontiers2016.' + p for p in find_packages('spaun2.0')],
     package_dir={'gosmann_frontiers2016._spaun': 'spaun2.0/_spaun'},
+    package_data={
+        'gosmann_frontiers2016._spaun.modules.vision': ['*.npz', '*.gz', '*.pkl'],
+        'gosmann_frontiers2016._spaun.modules.motor': ['*.npz', '*.gz', '*.pkl']},
 )
